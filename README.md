0e5c6a423edbcf64482c9dad48c4014f2034b8eb
shikshago <cscbob.dehpa@gmail.com>
2021-02-22 12:59:57 +0530

    v-0.2

--- README.md
+++ README.md
@@ -1,5 +1,50 @@
-It is The Demo Package Created For Educational Purpose,
-It Has one Classes inside
+Master Programing is a PIP Package that enables people that aren’t well to Build Software By Coding. Masterprograming.com provides all the Build Software for ABSOLUTELY FREE OF COST.
 
-class Master
-	method add_numbers(num1,num2)
+It is a Package Created For Educational Purpose,
+It Has one Classes inside Master.
+It Has 10 Methods inside.
+
+
+MPHindi - For Learning Python
+
+
+MPSimpleGui - Tkinter Calculator App
+
+
+MPPnr - PNR Checker App
+
+
+MPKeyboard - Onscreen Keyboard App
+
+
+MPCal - Calculator To Add Numbers
+
+
+MPQuiz - Quiz App
+
+
+MPSciCal - Scientific Calculator App
+
+
+MPApp - Tkinter Login And Signup With Database
+
+
+MPTodoApp - Todo list App
+
+
+---------------------------------------
+
+
+from masterprograming.master import *
+
+---------------------------------------
+obj = Master()
+
+--------------------------------------
+
+obj.MPSciCal()
+
+----------------------------------------
+
+
+
--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
-import setuptools
+from setuptools import setup , find_packages
+
+classifiers = [
+	"Development Status :: 5 - Production/Stable",
+	"Intended Audience :: Education",
+	"Operating System :: Microsoft :: Windows :: Windows 10",
+	"License :: OSI Approved :: MIT License",
+	"Programming Language :: Python :: 3",
+	]
 
 with open("README.md", "r") as fh:
 	long_description = fh.read()
 
-setuptools.setup(
-	name = "MasterPrograming",
-	version="0.0.1",
+setup(
+	name = "masterprograming",
+	version="0.0.5",
 	author = "Danish Ali",
 	author_email = "help@masterprograming.com",
-	description = "A small mathematics library",
+	description = "Master Programing is a PIP Package that enables people that aren’t well to Build Software By Coding",
 	long_description = long_description,
 	long_description_content_type = "text/markdown",
 	url = "",
-	packages= setuptools.find_packages(),
-	classifiers=[
-		"Programming Language :: Python :: 3",
-		"License :: OSI Approved :: MIL License",
-		"Operating System :: OS Independent"
-	],
+	packages= find_packages(),
+	classifiers=classifiers,
 	python_requires='>=3.6',
 )	
